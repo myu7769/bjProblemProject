5504277918386a351749d151512dd06ee2fe330b
myu7769 <122536664+myu7769@users.noreply.github.com>
2023-04-10 22:33:51 +0900

    [level 1] Title: 모의고사, Time: 3.35 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+
+        int[] stu1 = {1,2,3,4,5};       
+        int[] stu2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] stu3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+
+        PriorityQueue<Student> queue = new PriorityQueue<>((x,y)->y.cnt == x.cnt ? x.name-y.name : y.cnt-x.cnt);
+        ArrayList<Integer> list = new ArrayList<>();
+              
+        for(int i=0;i<answers.length; i++){
+            int cur = answers[i];
+            
+            if(cur == stu1[i%stu1.length]){
+                cnt1++;
+            }
+            if(cur == stu2[i%stu2.length]){
+                cnt2++;
+            }
+            if(cur == stu3[i%stu3.length]){
+                cnt3++;
+            }            
+        }
+        input(queue,new Student(1,cnt1));
+        input(queue,new Student(2,cnt2));
+        input(queue,new Student(3,cnt3));                
+        
+        int idx = 0;
+        int precnt  = 0;
+        while(!queue.isEmpty()){
+            Student cur = queue.poll();
+            if(idx != 0 && cur.cnt != precnt){
+                break;
+            }
+            idx++;
+            precnt = cur.cnt;
+            list.add(cur.name);
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+    public void input(PriorityQueue<Student> queue, Student stu){
+        if(stu.cnt != 0){            
+            queue.offer(stu);
+        }        
+    }
+    class Student {
+        int name;
+        int cnt;
+        Student(int name,int cnt){
+            this.name = name;
+            this.cnt = cnt;
+        }
+    }
+}
