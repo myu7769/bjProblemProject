b47f48fe5acd0b9fd80e180d1054c7728939f62c
myu7769 <122536664+myu7769@users.noreply.github.com>
2023-04-16 04:02:10 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,13 +1,30 @@
-import java.util.Arrays;
-import java.util.stream.LongStream;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(int[] lottos, int[] winNums) {
-        return LongStream.of(
-                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l) || l == 0).count(),
-                (lottos.length + 1) - Arrays.stream(lottos).filter(l -> Arrays.stream(winNums).anyMatch(w -> w == l)).count()
-        )
-                .mapToInt(op -> (int) (op > 6 ? op - 1 : op))
-                .toArray();
+    public int[] solution(int[] lottos, int[] win_nums) {
+        Set<Integer> set = new HashSet<>();
+        int zeroCount = 0;
+
+        for(int lotto : lottos) {
+            if(lotto == 0) {
+                zeroCount++;
+                continue;
+            }
+            set.add(lotto);
+        }
+
+
+        int sameCount = 0;
+        for(int winNum : win_nums) {
+            if(!set.add(winNum)) sameCount++;
+        }
+
+        int maxRank = 7 - (sameCount + zeroCount);
+        int minRank = 7 - sameCount;
+        if(maxRank > 6) maxRank = 6;
+        if(minRank > 6) minRank = 6;
+
+        return new int[] {maxRank, minRank};
     }
 }
