1656f8660676c44af404799ae5ba3899330f0d15
myu7769 <122536664+myu7769@users.noreply.github.com>
2023-04-21 16:10:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.01 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,17 @@
-import java.util.*;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];   
-            }            
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
         }
-        
-        return participant[participant.length - 1];
+        return answer;
     }
 }
